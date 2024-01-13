12f7d6b12a1ab5e88d4bc39a7bd821998177e7f8
Noura-Darwazeh <noura.darwazeh11@gmail.com>
2024-01-13 15:39:29 -0500

    services component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Header from './components/header/Header';
 import Slider from './components/slider/Slider';
+import Services from './components/services/Services';
 
 function App() {
   return (
     <div>
       <Header />
       <Slider />
+      <Services />
     </div>
   );
 }
